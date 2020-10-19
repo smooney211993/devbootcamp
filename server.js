f0fe24a9d36966b4eeb44395f7342d0269736871
smooney211993 <sm.athome21@gmail.com>
2020-10-19 16:46:25 +1100

    loaded env variables

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
-const dotenb = require('dotenv');
+const dotenv = require('dotenv');
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+dotenv.config({ path: './config/config.env' });
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
