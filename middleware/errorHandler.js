9c1a758c2892e4d5ed905830633a5455e313e857
smooney211993 <sm.athome21@gmail.com>
2020-10-20 16:01:33 +1100

    changed custom error handler to send different status codes and messages dependding on the error

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,8 +1,16 @@
 const errorHandler = (err, req, res, next) => {
-  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  let statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  let message;
+  if (err.code === 11000) {
+    statusCode = 400;
+  }
+  if (err.name === 'ValidationError') {
+    statusCode = 400;
+    message = Object.values(err.errors).map((val) => val.message);
+  }
   res.status(statusCode);
   res.json({
-    message: err.message,
+    message: err.message || message,
     stack: process.env.NODE_ENV === 'production' ? null : err.stack,
   });
 };
