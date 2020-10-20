8328065141b035ba858d6cc598c85c88d2ceed23
smooney211993 <sm.athome21@gmail.com>
2020-10-20 12:34:41 +1100

    created notfound error handler

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -6,7 +6,12 @@ const errorHandler = (err, req, res, next) => {
     stack: process.env.NODE_ENV === 'production' ? null : err.stack,
   });
 };
-
+const notFound = (req, res, next) => {
+  const error = new Error(`Not Found - ${req.originalUrl}`);
+  res.status(404);
+  next(error);
+};
 module.exports = {
   errorHandler,
+  notFound,
 };
