abde18e990b02ad4e1faa81cc159adf4b24c3549
smooney211993 <sm.athome21@gmail.com>
2020-10-21 00:06:31 +1100

    added geocoder to run presave of data entry on the bootcamp model

--- models/Bootcamps.js
+++ models/Bootcamps.js
@@ -107,5 +107,21 @@ const BootcampSchema = new mongoose.Schema(
 );
 
 // Geocode & create location field
+BootcampSchema.pre('save', async function (next) {
+  const loc = await geocoder.geocode(this.address);
+  this.location = {
+    type: 'Point',
+    coordinates: [loc[0].longitude, loc[0].latitude],
+    formattedAddress: loc[0].formattedAddress,
+    street: loc[0].streetName,
+    city: loc[0].city,
+    state: loc[0].stateCode,
+    zipcode: loc[0].zipcode,
+    country: loc[0].countryCode,
+  };
+
+  this.address = undefined;
+  next();
+});
 
 module.exports = Bootcamp = mongoose.model('Bootcamp', BootcampSchema);
