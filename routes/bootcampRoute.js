edc24b332dfa1db6d9062443c9def4e0327c6299
smooney211993 <sm.athome21@gmail.com>
2020-10-20 11:56:40 +1100

    added getbootcamps function in bootcamps route

--- routes/bootcampRoute.js
+++ routes/bootcampRoute.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { createNewBootCamp } = require('../controllers/bootcampController');
+const {
+  createNewBootCamp,
+  getBootCamps,
+} = require('../controllers/bootcampController');
 
-router.route('/').post(createNewBootCamp);
+router.route('/').post(createNewBootCamp).get(getBootCamps);
 
 module.exports = router;
