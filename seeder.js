195aaafa6ac98950f8e486e53b7fa8062a1e099a
smooney211993 <sm.athome21@gmail.com>
2020-10-21 08:27:54 +1100

    created seeder import and delete function

---
+++ seeder.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+const Bootcamp = require('./models/Bootcamps');
+
+mongoose.connect(process.env.MONGO_URI, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+
+const bootcamps = JSON.parse(
+  fs.readFileSync(`${__dirname}/data/bootcamps.json`, 'utf-8')
+);
+
+const importData = async () => {
+  try {
+    await Bootcamp.create(bootcamps);
+    console.log('Data Imported');
+    process.exit();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const deleteData = async () => {
+  try {
+    await Bootcamp.deleteMany();
+    process.exit();
+  } catch (error) {
+    console.log(error);
+  }
+};
