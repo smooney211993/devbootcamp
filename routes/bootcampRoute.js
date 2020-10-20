b2eafe156aae24cb78d05de4bdd888154e0fb92e
smooney211993 <sm.athome21@gmail.com>
2020-10-20 12:00:32 +1100

    added a new route that handles params of id and uses the getbootcampbyid function

--- routes/bootcampRoute.js
+++ routes/bootcampRoute.js
@@ -3,8 +3,10 @@ const router = express.Router();
 const {
   createNewBootCamp,
   getBootCamps,
+  getBootCampById,
 } = require('../controllers/bootcampController');
 
 router.route('/').post(createNewBootCamp).get(getBootCamps);
+router.route('/:id').get(getBootCampById);
 
 module.exports = router;
