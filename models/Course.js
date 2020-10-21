f20fc7e35c0ca171e8e3d79db197dc8b34a3598c
smooney211993 <sm.athome21@gmail.com>
2020-10-21 16:00:41 +1100

    created the course model

---
+++ models/Course.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+
+const CourseSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    trim: true,
+    required: [true, 'Please Add A Course Title'],
+  },
+  description: {
+    type: String,
+    required: [true, 'Please Add A Description'],
+  },
+  weeks: {
+    type: String,
+    required: [true, 'Please Add Number Of Weeks'],
+  },
+  tuition: {
+    type: Number,
+    required: [true, 'Please Add Cost'],
+  },
+  minimumSkill: {
+    type: String,
+    required: [true, 'Please Add Minimum Skill '],
+    enum: ['beginner', 'intermediate', 'advanced'],
+  },
+  scholarshipAvailable: {
+    type: Boolean,
+    default: false,
+  },
+  createdAt: {
+    type: Data,
+    default: Date.now,
+  },
+  bootcamp: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'Bootcamp',
+    required: true,
+  },
+});
+
+module.exports = Course = mongoose.model('Course', CourseSchema);
