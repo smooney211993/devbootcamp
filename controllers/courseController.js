cedf1d1d7bad3f32c1328308eb6a356c37c4b0e5
smooney211993 <sm.athome21@gmail.com>
2020-10-21 17:00:07 +1100

    created getcourses function in course controller

---
+++ controllers/courseController.js
@@ -0,0 +1,21 @@
+const asyncHandler = require('express-async-handler');
+const Course = require('../models/Course');
+
+// Get courses
+// Get /api/v1/courses
+// public
+
+const getCourses = asyncHandler(async (req, res) => {
+  let query;
+  if (req.params.bootCampId) {
+    query = Course.find({ bootcamp: req.params.id });
+  } else {
+    query = Course.find();
+  }
+  const courses = await query;
+  res.status(200).json({
+    success: true,
+    count: courses.length,
+    data: courses,
+  });
+});
