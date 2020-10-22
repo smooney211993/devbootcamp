9f8060719f894412d6215441d6cb060510af3611
smooney211993 <sm.athome21@gmail.com>
2020-10-22 11:43:53 +1100

    added updatecourse in route

--- routes/courseRoute.js
+++ routes/courseRoute.js
@@ -3,10 +3,11 @@ const {
   getCourses,
   getCourseById,
   createCourse,
+  updateCourse,
 } = require('../controllers/courseController');
 const router = express.Router({ mergeParams: true });
 
 router.route('/').get(getCourses).post(createCourse);
-router.route('/:id').get(getCourseById);
+router.route('/:id').get(getCourseById).put(updateCourse);
 
 module.exports = router;
