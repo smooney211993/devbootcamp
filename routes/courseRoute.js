91995527a399453d0d04335c77e1caec2c0c2014
smooney211993 <sm.athome21@gmail.com>
2020-10-22 11:47:58 +1100

    added deleteCourse in courseroute

--- routes/courseRoute.js
+++ routes/courseRoute.js
@@ -4,10 +4,11 @@ const {
   getCourseById,
   createCourse,
   updateCourse,
+  deleteCourse,
 } = require('../controllers/courseController');
 const router = express.Router({ mergeParams: true });
 
 router.route('/').get(getCourses).post(createCourse);
-router.route('/:id').get(getCourseById).put(updateCourse);
+router.route('/:id').get(getCourseById).put(updateCourse).delete(deleteCourse);
 
 module.exports = router;
