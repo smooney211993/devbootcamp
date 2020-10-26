f0bb44dd753306ffd5beca016ba12ad30c36db66
smooney211993 <sm.athome21@gmail.com>
2020-10-26 18:23:43 +1100

    created middleware that checks for jwt token

---
+++ middleware/authToken.js
@@ -0,0 +1,22 @@
+const authToken = (req, res, next) => {
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    try {
+      token = token = req.headers.authorization.split(' ')[1];
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      req.user = decoded.user;
+      next();
+    } catch (error) {
+      res.status(401).json({ msg: 'token is not valid' });
+      // if token does exist but however valid send this error message
+    }
+  }
+  if (!token) {
+    return res.status(401).json({ msg: 'No token, authorization not granted' });
+  }
+};
+
+module.exports = authToken;
