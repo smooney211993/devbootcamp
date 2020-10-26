8e0aaa22eaf9a8303191a4c4c66211f84ac668b4
smooney211993 <sm.athome21@gmail.com>
2020-10-26 19:16:31 +1100

    changed the error handling in the auth token to throw new errors

--- middleware/authToken.js
+++ middleware/authToken.js
@@ -1,18 +1,26 @@
 const jwt = require('jsonwebtoken');
+const asyncHandler = require('express-async-handler');
 const authToken = (req, res, next) => {
   let token;
+
   if (
     req.headers.authorization &&
     req.headers.authorization.startsWith('Bearer')
   ) {
-    token = token = req.headers.authorization.split(' ')[1];
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded.user;
-    next();
+    try {
+      token = token = req.headers.authorization.split(' ')[1];
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      req.user = decoded.user;
+      next();
+    } catch (error) {
+      res.status(401);
+      throw new Error('Token Is Not Valid');
+      // if token does exist but however valid send this error message
+    }
   }
   if (!token) {
-    res.status(201);
-    throw new Error('Authorization Not Granted');
+    res.status(401);
+    throw new Error('No token, Authorization not granted');
   }
 };
 
