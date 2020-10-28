eaa9470e6979997fc520e1d6c5f6265a08be80a3
smooney211993 <sm.athome21@gmail.com>
2020-10-28 13:19:22 +1100

    created get userbyidasadmin function

--- controllers/userController.js
+++ controllers/userController.js
@@ -8,6 +8,18 @@ const getUsersAsAdmin = asyncHandler(async (rec, res) => {
   res.status(200).json(res.advanceResults);
 });
 
+const getUserByIdAsAdmin = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id);
+  if (!user) {
+    res.status(400);
+    throw new Error('User Does Not Exist');
+  }
+  res.status(200).json({
+    success: true,
+    data: user,
+  });
+});
 module.exports = {
   getUsersAsAdmin,
+  getUserByIdAsAdmin,
 };
