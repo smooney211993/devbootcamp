9d5b41f59588616a9b08317ca860508634c65f8d
smooney211993 <sm.athome21@gmail.com>
2020-11-02 12:50:16 +1100

    created a watcher function for the userlogin saga

--- client/src/sagas/userLoginSaga.js
+++ client/src/sagas/userLoginSaga.js
@@ -2,6 +2,26 @@ import {
   USER_LOGIN_REQUEST,
   USER_LOGIN_SUCCESS,
   USER_LOGIN_FAIL,
+  LOAD_USER_REQUEST,
+  LOAD_USER_SUCCESS,
+  LOAD_USER_FAIL,
 } from '../actions/types';
-
+import { fetchUserToken, fetchUser } from '../api/user';
 import { call, put, takeEvery } from 'redux-saga/effects';
+
+function* getUserAccessToken(action) {
+  const { email, password } = action.payload;
+  try {
+    const token = yield call(fetchUserToken, email, password);
+    yield put({ USER_LOGIN_SUCCESS, payload: token });
+  } catch (error) {
+    yield put({
+      USER_LOGIN_FAIL,
+      payload: { msg: error.response.statusText, err: error.response.status },
+    });
+  }
+}
+
+export default function* userLoginSaga() {
+  yield takeEvery(USER_LOGIN_REQUEST, getUserAccessToken);
+}
