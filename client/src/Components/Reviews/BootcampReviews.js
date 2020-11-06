8d2d82ab389d8f455cf60da429f937c88e53dd86
smooney211993 <sm.athome21@gmail.com>
2020-11-06 14:28:56 +1100

    created reviewbootcamp component

--- client/src/Components/Reviews/BootcampReviews.js
+++ client/src/Components/Reviews/BootcampReviews.js
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { getBootCampReview } from '../../actions/bootcampActions';
+import { useDispatch, useSelector } from 'react-redux';
 
-const BootcampReviews = () => {
-  return <div>Reviews</div>;
+import Message from '../Layout/Message';
+import Spinner from '../Layout/Spinner';
+
+const BootcampReviews = ({ match }) => {
+  const id = match.params.id;
+  const dispatch = useDispatch();
+  const { reviews, loading, error } = useSelector(
+    (state) => state.bootcampReviews
+  );
+
+  useEffect(() => {
+    dispatch(getBootCampReview(id));
+  }, [dispatch, id]);
+  return error ? (
+    <Message>{error.msg}</Message>
+  ) : loading ? (
+    <Spinner />
+  ) : (
+    <>Reviews</>
+  );
 };
 
 export default BootcampReviews;
