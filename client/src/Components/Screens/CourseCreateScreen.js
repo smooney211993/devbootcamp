28411a8356fabe275e7ffa77acee1b281f8bcc02
smooney211993 <sm.athome21@gmail.com>
2020-11-24 17:07:24 +1100

    created coursecreate screen

--- client/src/Components/Screens/CourseCreateScreen.js
+++ client/src/Components/Screens/CourseCreateScreen.js
@@ -10,6 +10,12 @@ const CourseCreateScreen = () => {
   const [bootcamps, setBootcamps] = useState([]);
   const [formData, setFormData] = useState({
     bootcamp: '',
+    title: '',
+    description: '',
+    weeks: '',
+    tuition: '',
+    minimumSkill: '',
+    scholarhipsAvailable: null,
   });
 
   useEffect(() => {
@@ -70,6 +76,93 @@ const CourseCreateScreen = () => {
                   ))}
               </Form.Control>
             </Form.Group>
+            <Form.Group>
+              <Form.Label>Title</Form.Label>
+              <Form.Control
+                type='text'
+                placeholder='Please Enter Title Of Course'
+                value={formData.title}
+                name='title'
+                onChange={(e) =>
+                  setFormData({ ...formData, [e.target.name]: e.target.value })
+                }></Form.Control>
+            </Form.Group>
+            <Form.Group>
+              <Form.Label>Description</Form.Label>
+              <Form.Control
+                as='textarea'
+                placeholder='Please Enter Description Of Course'
+                rows={6}
+                name='description'
+                value={formData.value}
+                onChange={(e) =>
+                  setFormData({ ...formData, [e.target.name]: e.target.value })
+                }></Form.Control>
+            </Form.Group>
+            <Form.Group controlId='weeks'>
+              <Form.Label>Weeks</Form.Label>
+              <Form.Control
+                type='number'
+                value={formData.weeks}
+                name='weeks'
+                onChange={(e) =>
+                  setFormData({ ...formData, [e.target.name]: e.target.value })
+                }></Form.Control>
+            </Form.Group>
+            <Form.Group controlId='tuition'>
+              <Form.Label>Tuition Cost</Form.Label>
+              <Form.Control
+                type='number'
+                value={formData.tuition}
+                name='tuition'
+                onChange={(e) =>
+                  setFormData({ ...formData, [e.target.name]: e.target.value })
+                }></Form.Control>
+            </Form.Group>
+            <Form.Group controlId='minimumskill'>
+              <Form.Label>Skill Level Required</Form.Label>
+              <Form.Control
+                as='select'
+                name='minimumSkill'
+                value={formData.minimumSkill}
+                onChange={(e) =>
+                  setFormData({ ...formData, [e.target.name]: e.target.value })
+                }>
+                {['beginner', 'intermediate', 'advanced'].map((x, i) => (
+                  <option key={i} value={x}>
+                    {x}
+                  </option>
+                ))}
+              </Form.Control>
+            </Form.Group>
+            <Form.Group>
+              <Form.Label>Scholarship Available</Form.Label>
+              <Form.Check
+                label='True'
+                type='radio'
+                id='True'
+                name='scholarhipsAvailable'
+                onChange={(e) =>
+                  setFormData({ ...formData, [e.target.name]: true })
+                }
+                checked={
+                  formData.scholarhipsAvailable ? true : false
+                }></Form.Check>
+              <Form.Check
+                label='False'
+                type='radio'
+                id='False'
+                name='scholarhipsAvailable'
+                onChange={(e) =>
+                  setFormData({ ...formData, [e.target.name]: false })
+                }
+                checked={
+                  !formData.scholarhipsAvailable ? true : false
+                }></Form.Check>
+            </Form.Group>
+            <Button variant='primary' type='submit'>
+              Submit
+            </Button>
           </Col>
         </Row>
       </Container>
